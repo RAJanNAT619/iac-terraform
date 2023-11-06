9f3a849449f444d6d1b6aa048afb48db175dc937
natraj <41659947+RAJanNAT619@users.noreply.github.com>
2023-11-06 11:57:45 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -121,5 +121,18 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 }
 
+# Create an RDS Instance
+resource "aws_db_instance" "example" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "username"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+
+}
 
 
