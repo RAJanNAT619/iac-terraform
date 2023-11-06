061a6dd29bdb66954c12ebf48d3e077810da627b
natraj <41659947+RAJanNAT619@users.noreply.github.com>
2023-11-06 11:54:42 +0530

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+# Creating a New Key
+resource "tls_private_key" "rsa_4096" {
+   algorithm = "RSA"
+   rsa_bits  = 4096
+}
+
+variable "key_name" {}
+
+resource "aws_key_pair" "key_pair" {
+   key_name = var.key_name
+   public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+   content = tls_private_key.rsa_4096.private_key_pem
+   filename = var.key_name
+}
